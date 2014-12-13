25aa9702a9329d5eb6586c592c3fd05293aa8e84
David Abad Vich <davidabad10@gmail.com>
2014-12-13 19:19:53 +0100

    collision pot

---
+++ serious scripts/CollisionTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Rotura de un objeto prueba, su animador necesita la variable de rotura "isBroken"
+public class CollisionTest : MonoBehaviour {
+
+	private Animator animator;
+	private CircleCollider2D cc;
+
+	void Start () {
+		animator = this.GetComponent<Animator>();
+		cc = this.GetComponent<CircleCollider2D>();
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Debug.Log ("Objeto triger entra");
+		if(!other.gameObject.name.Equals("Player"))
+			if (animator.GetBool ("isBroken") == false) {
+				animator.SetBool ("isBroken", true);
+				Destroy (other.gameObject);
+				cc.enabled = false;
+			}
+	}
+}
